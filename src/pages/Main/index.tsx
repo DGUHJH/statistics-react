13b8bb30a7bb9067323502d62a8740674ed63e5e
DGUHJH <ghkdwogud852@gmail.com>
2022-09-19 16:41:17 +0900

    Feat : 기능 활성화

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,208 @@
-import React from 'react';
-import { Root } from './styled';
+import { message, Table } from 'antd';
+import React, { useEffect, useState } from 'react';
+import {
+  Bar,
+  Box,
+  BoxContainer,
+  ChipContainer,
+  ChipTitleTypo,
+  Container,
+  ContentButton,
+  ContentButtonTypo,
+  ContentChip,
+  ContentInput,
+  ContentTypo,
+  Empty,
+  InfoContainer,
+  InputContainer,
+  Root,
+} from './styled';
+
+const columns = [
+  {
+    title: 'Schema',
+    dataIndex: 'schema',
+    key: 'schema',
+  },
+  {
+    title: 'Value',
+    dataIndex: 'value',
+    key: 'value',
+  },
+];
 
 const Main = () => {
-  return <Root></Root>;
+  const [value, setValue] = useState<string>();
+  const [dataList, setDataList] = useState<number[]>(
+    [
+      12.81, 14.95, 15.83, 19.9, 18.34, 19.82, 19.94, 20.62, 36.73, 20.88,
+      20.93, 20.98, 21.15, 22.24, 23.16, 22.24, 23.16, 23.56, 35.78,
+    ].sort()
+  );
+
+  const onClickDeleteData = (id: number) => () => {
+    setDataList((prev) => prev.filter((value, index) => index !== id));
+  };
+
+  const onClickAddData = () => {
+    if (value) {
+      if (isNaN(+value)) {
+        message.error('number을 입력해주세요.');
+        setValue(undefined);
+        return;
+      }
+      setDataList((prev) => [...prev, +value].sort());
+      setValue(undefined);
+      return;
+    }
+    message.error('데이터를 입력해주세요.');
+  };
+
+  const onPressEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      onClickAddData();
+      return;
+    }
+  };
+
+  const getQ = (type: 'Q1' | 'M' | 'Q3') => {
+    const dataLength = dataList.length;
+    if (type === 'Q1') {
+      return (
+        (dataList[Math.floor((dataLength * 1) / 4)] +
+          dataList[Math.floor((dataLength * 1) / 4) - 1]) /
+        2
+      );
+    }
+    if (type === 'M') {
+      return (
+        (dataList[Math.floor((dataLength * 2) / 4)] +
+          dataList[Math.floor((dataLength * 2) / 4) - 1]) /
+        2
+      );
+    }
+    return (
+      (dataList[Math.floor((dataLength * 3) / 4)] +
+        dataList[Math.floor((dataLength * 3) / 4) - 1]) /
+      2
+    );
+  };
+
+  const iqr = getQ('Q3') - getQ('Q1');
+  const right = getQ('Q3') + iqr * 1.5;
+  const left = getQ('Q1') - iqr * 1.5;
+
+  const min = (() => {
+    let result = 0;
+    let check = true;
+    let condition = left;
+    dataList.forEach((dataItem, index) => {
+      if (dataItem >= condition && check) {
+        index !== dataList.length - 1 && (result = index + 1);
+        check = false;
+      }
+    });
+    return dataList[result];
+  })();
+
+  const max = (() => {
+    let result = 0;
+    let condition = right;
+    dataList.forEach((dataItem, index) => {
+      if (dataItem < condition) {
+        index !== 0 && (result = index - 1);
+      }
+    });
+    return dataList[result];
+  })();
+
+  const dataSource = [
+    {
+      key: '1',
+      schema: 'Q1',
+      value: getQ('Q1'),
+    },
+    {
+      key: '2',
+      schema: 'M',
+      value: getQ('M'),
+    },
+    {
+      key: '3',
+      schema: 'Q3',
+      value: getQ('Q3'),
+    },
+    {
+      key: '4',
+      schema: 'IQR (Q3 - Q1)',
+      value: iqr.toFixed(2),
+    },
+    {
+      key: '5',
+      schema: '좌측경계',
+      value: left.toFixed(2),
+    },
+    {
+      key: '6',
+      schema: '우측경계',
+      value: right.toFixed(2),
+    },
+    {
+      key: '7',
+      schema: 'min',
+      value: min.toFixed(2),
+    },
+    {
+      key: '8',
+      schema: 'max',
+      value: max.toFixed(2),
+    },
+  ];
+
+  return (
+    <Root>
+      <Container>
+        <InfoContainer>
+          <ContentTypo>
+            ex28) 다음 자료는 중년 남성의 20명의 폐활량을 조사한 것이다.
+            상자그림을 그리시오.
+          </ContentTypo>
+        </InfoContainer>
+        <ChipContainer>
+          <ChipTitleTypo level={4}>데이터 목록</ChipTitleTypo>
+          {dataList.map((dataItem, index) => (
+            <ContentChip
+              onClick={onClickDeleteData(index)}
+              key={`data_item_${index}`}
+            >
+              {dataItem}
+            </ContentChip>
+          ))}
+        </ChipContainer>
+        <InputContainer>
+          <ContentInput
+            placeholder='데이터를 입력해주세요.'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={onPressEnter}
+          />
+          <ContentButton onClick={onClickAddData}>
+            <ContentButtonTypo>추가</ContentButtonTypo>
+          </ContentButton>
+        </InputContainer>
+        <BoxContainer>
+          <Empty width={(((min - left) / iqr) * 300) / 4} />
+          <Bar width={(((getQ('Q1') - min) / iqr) * 300) / 4} />
+          <Box />
+          <Bar width={(((max - getQ('Q3')) / iqr) * 300) / 4} />
+          <Empty width={(((right - max) / iqr) * 300) / 4} />
+        </BoxContainer>
+        <InfoContainer>
+          <Table dataSource={dataSource} columns={columns} pagination={false} />
+        </InfoContainer>
+      </Container>
+    </Root>
+  );
 };
 
 export default Main;
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,3 +1,103 @@
+import { Button, Input, InputNumber, Typography } from 'antd';
 import styled from 'styled-components';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 200px;
+`;
+
+export const Container = styled.div`
+  width: 320px;
+  margin-top: 20px;
+`;
+
+export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+`;
+
+export const ContentTypo = styled(Typography)``;
+
+export const ContentInput = styled(Input)``;
+
+export const ContentButton = styled(Button)``;
+export const ContentButtonTypo = styled(Typography)``;
+
+export const ChipContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  padding: 20px;
+  box-sizing: border-box;
+  border: 1px #0003 solid;
+  border-radius: 5px;
+  margin: 20px 0;
+`;
+
+export const ChipTitleTypo = styled(Typography.Title)`
+  width: 100%;
+  text-align: center;
+`;
+
+export const ContentChip = styled.div`
+  background: #0f02;
+  padding: 5px 10px;
+  border: 1px #0f05 solid;
+  border-radius: 5px;
+  box-sizing: border-box;
+  cursor: pointer;
+  :hover {
+    background: #f002;
+    border: 1px #f005 solid;
+  }
+`;
+
+export const BoxContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px #0003 solid;
+  margin-top: 20px;
+`;
+
+type EmptyProps = {
+  width: number;
+};
+
+export const Empty = styled.div<EmptyProps>`
+  width: ${(props) => props.width}px;
+  height: calc(300px / 4);
+  background: #f003;
+`;
+
+type BarProps = {
+  width: number;
+};
+
+export const Bar = styled.div<BarProps>`
+  width: ${(props) => props.width}px;
+  height: 2px;
+  background: #c9c9c9;
+`;
+
+export const Box = styled.div`
+  width: calc(300px / 4);
+  height: calc(300px / 4);
+  border: 2px #c9c9c9 solid;
+`;
